97ea4329a002773b5bd38252801503b151ecdf29
Lee Yeseul <80022494+yes-ee@users.noreply.github.com>
2023-03-24 22:29:54 +0900

    cpp module 00 ex00

---
+++ CPP/CPP Module 00/ex00/megaphone.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+
+	for (int i = 1; i < argc; i++)
+	{
+		for (size_t j = 0; j < strlen(argv[i]); j++)
+		{
+			std::cout << static_cast<char>(toupper(argv[i][j]));
+		}
+	}
+	std::cout << std::endl;
+
+	return 0;
+}
