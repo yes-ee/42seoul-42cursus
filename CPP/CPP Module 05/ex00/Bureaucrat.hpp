1a54b56e431927de496c2b93999ed4e3d5664046
Lee Yeseul <80022494+yes-ee@users.noreply.github.com>
2023-06-05 22:14:36 +0900

    CPP Module 05 ex00 진행 중

---
+++ CPP/CPP Module 05/ex00/Bureaucrat.hpp
@@ -0,0 +1,16 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <string>
+
+class Bureaucrat {
+	private:
+		const std::string name;
+		int grade;
+	public:
+		class GradeTooHighException {
+			
+		}
+}
+
+#endif
